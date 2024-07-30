082391142365d0a1c98c096e26b3c522ffd49104
ssafy <ssafy@naver.com>
2024-07-30 18:00:53 +0900

    dd

--- Study/src/s0730/minecraft.java
+++ Study/src/s0730/minecraft.java
@@ -1,42 +1,52 @@
 package s0730;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class minecraft {
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int B = sc.nextInt();
-		
-		long[][] g = new long[N][M];
-		double sum=0;
+		int min = 256;
+		int max = 0;
+		int[][] g = new int[N][M];
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
 				g[i][j]=sc.nextInt();
-				sum+=g[i][j];
+				max = Math.max(max,g[i][j]);
+				min = Math.min(min, g[i][j]);
 			}
 		}
-		long avg = Math.round(sum/(N*M));
-		int time = 0;
+		int T=0;
+		int height=0;
+		int[][] check = new int[max-min+1][2];
+		for(int k=min;k<=max;k++) {
+			int time=0;
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<M;j++) {
+					if(g[i][j]>k) {
+						time += (g[i][j]-k)*2;
+						B+=(g[i][j]-k);
+					}
+					else if(g[i][j]<k){
+						time += (k-g[i][j]);
+						B-=(k-g[i][j]);
+					}
+				}
+			}
+			
+			if(B>=0){
+				if(height<k) {
+					T = time;
+					height=k;
+				}
+			}
+		}
+		System.out.println(T+" "+height);
 		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				if(g[i][j]==avg) {
-					continue;
-				}
-				else if(g[i][j]>avg) {
-					time += (g[i][j]-avg)*2;
-				}
-				else {
-					time += (avg-g[i][j]);
-				}
-			}
-		}
-		System.out.println(time+" "+avg);
+		
 
 	}
 
