04a029accee60a2110d4a5694f0171628a5a22e4
taeyoung <teayoung0812@naver.com>
2024-08-11 23:56:38 +0900

    0811

--- Study/src/s0808/LoveGCD.java
+++ Study/src/s0808/LoveGCD.java
@@ -20,8 +20,16 @@ public class LoveGCD {
 		for(int i=0;i<N;i++) {
 			arr.add(sc.nextInt());
 		}
+		
+		arrS(0,N-1);
 
 	}
+	public static int arrS(int start, int end) {
+		if(start == end) return arr.get(start);
+		
+		return 0;
+	}
+	
 	// 배열 S의 원소를 왼쪽부터 N/2개 선택
 	public static void leftarr(int count) {
 		for(int i=0;i<count;i++) {
@@ -37,6 +45,7 @@ public class LoveGCD {
 		}
 	}
 	// 선택한 원소들의 GCD 구함
+	/*
 	public static void GCD(ArrayList<Integer> check) {
 		for(int i=0;i<check.size()-2;i++) {
 			if(check.get(i)>check.get(i+1)) {
@@ -48,6 +57,72 @@ public class LoveGCD {
 				}
 			}
 		}
-	}
+	}*/
+	// gcd 구하기
+	public static int GCD(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return GCD(num2, num1 % num2);
+    }
 
 }
+
+/*
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int sum;
+    static LinkedList<Integer> room;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        room = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            room.add(Integer.parseInt(st.nextToken()));
+        }
+
+        func(0, n-1);
+        System.out.println(sum);
+    }
+
+    public static int func(int start, int end) {
+        if(start == end) return room.get(start);
+        else if(start+1 == end) return room.get(start) + room.get(end);
+
+        int mid = (end - start + 1) / 2;
+        int gcd = room.get(start);
+
+        int leftSum = 0;
+        for(int i = start; i < mid; i++) {
+            gcd = gcd(gcd, room.get(i));
+        }
+        leftSum = gcd + func(start + mid, end);
+
+        gcd = room.get(end);
+        int rightSum = 0;
+        for(int i = mid; i < end; i++) {
+            gcd = gcd(gcd, room.get(i));
+        }
+        rightSum = gcd + func(start, start + mid - 1);
+
+        return Math.max(leftSum, rightSum);
+    }
+
+    public static int gcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
+    }
+}*/
+
+
+
+
+
