e4a12cf697772d394f04b540f9c8d951384727db
taeyoung <teayoung0812@naver.com>
2024-08-31 23:51:46 +0900

    0831

--- Study/src/s0830/SWEA_Processor.java
+++ Study/src/s0830/SWEA_Processor.java
@@ -4,121 +4,134 @@ import java.io.*;
 import java.util.*;
 
 public class SWEA_Processor {
-	
+
 	static int N;
-	static int[][] arr;
-	//static int[] dx = {-1, 1, 0, 0};
-	//static int[] dy = {0, 0, 1,-1};
-	static ArrayList<Info> lst;
-	static int total;
-	static boolean[] visited;
-	
-	//static boolean[][] state;
-	
-	static class Info{
-		int r;
-		int c;
-		Info(int r, int c){
-			this.r = r;
-			this.c = c;
-		}
-	}
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static ArrayList<Info> cores;
+    static int maxCores, minLen;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st ;
-		
-		int T = Integer.parseInt(br.readLine());
-		for(int i=1;i<=T;i++) {
-			N = Integer.parseInt(br.readLine());
-			arr = new int[N][N];
-			
-			// 가장 자리에는 전원이 흐르고 있음
-			// 가장자리에 위치한 Core는 이미 전원이 연결된 것으로 간주
-			// Core와 전원을 연결하는 전선은 직선으로만 설치 가능
-			// 전선은 교차 안된다
-			// 전원이 연결되지 않는 Core가 존재할 수 있음
-			
-			// 최대한 많은 Core에 전원을 연결하였을 경우, 전선 길이의 합을 구하기
-			// 전선 길이의 합이 최소가 되는 값
-			
-			// 1은 core, 0은 빈 cell
-			// 1개의 cell에는 1개의 Core 혹은 1개의 전선이 올 수 있음
-			// Core의 개수는 1개 이상 12개 이하
-			
-			lst = new ArrayList<Info>();
-			
-			for(int r=1;r<=N;r++) {
-				st = new StringTokenizer(br.readLine());
-				for(int c=1;c<=N;c++) {
-					arr[r][c] = Integer.parseInt(st.nextToken());
-					// Core의 인덱스 정보 저장 
-				}
-			}
-			total=0;
-			
-			boolean[][] state = new boolean[N][N];
-			// visited, length, Core 개수
-			// 가장자리에 있는 Core를 제외하고 Core 개수 구하기
-			for(int r=1;r<N-1;r++) {
-				for(int c=1;c<N-1;c++) {
-					if(arr[r][c]==1) {
-						total++;
-						lst.add(new Info(r,c));
-					}
-				}
-			}
-			visited = new boolean[total];
-			
-			// 큰 개수부터 작아지면서 Core 뽑을 개수 구하기 
-			for(int j=total;j>total;j--) {
-				comb(0, j);
-			}
-			
-			// 조합으로 큰 개수부터 작은 개수로 뽑아가기 연결할 수 있으면 그 개수로
-			// 2차원 boolean 배열을 사용해서 전선을 연결된 경우, core가 있는위치로는 전원을 연결할 수없음을  나타내기
-			// 백트래킹으로 뽑아낸 Core마다 안되면 돌아가서 다른 방향 보고 4방향 다 확인해보고 다음 Core로 넘어감
-			
-		}
-	}
-	public static void comb(int idx, int M) {
-		if(idx == M) {
-			for(int i=0;i<total;i++) {
-				if(visited[i]) {
-					check(lst.get(i).r, lst.get(i).c);
-				}
-			}
-		}
-		for(int i=0;i<total;i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				comb(idx+1, M);
-				visited[i] = false;
-			}
-		}
-	}
-	
-	public static void check(int r, int c) {
-		
-		// 오른쪽 확인
-		for(int i=c+1;i<N;i++) {
-			if(arr[r][i] ==0) {
-				continue;
-			}
-		}
-		// 왼쪽 확인
-		
-		
-		// 위쪽 확인
-		
-		// 아래쪽 확인 
-		
-		
-		
-		
-		
-	}
-	
+    static class Info {
+        int r, c;
 
+        Info(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+            arr = new int[N][N];
+            cores = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    // 가장자리를 제외한 코어를 리스트에 추가 
+                    if (arr[i][j] == 1 && i != 0 && j != 0 && i != N - 1 && j != N - 1) {
+                        cores.add(new Info(i, j));
+                    }
+                }
+            }
+
+            maxCores = 0;
+            minLen = Integer.MAX_VALUE;
+
+            for (int i = cores.size(); i >= 1; i--) {
+                comb(0, 0, i, new boolean[cores.size()]);
+                // 해당 조합에서 최대 코어수를 찾았다면 더 이상 탐색할 필요가 없음
+                if (maxCores > 0) break;
+            }
+
+            System.out.println("#" + t + " " + minLen);
+        }
+    }
+
+    // 조합으로 연결할 코어 선택 
+    static void comb(int start, int cnt, int M, boolean[] selected) {
+        if (cnt == M) {
+            check(0, 0, 0, selected);
+            return;
+        }
+        for (int i = start; i < cores.size(); i++) {
+            selected[i] = true;
+            comb(i + 1, cnt + 1, M, selected);
+            selected[i] = false;
+        }
+    }
+
+    static void check(int index, int connected, int wireLen, boolean[] selected) {
+        // 모든 코어를 다 확인했을 때
+        if (index == cores.size()) {
+            if (connected > maxCores) {
+                maxCores = connected;
+                minLen = wireLen;
+            } else if (connected == maxCores) {
+                // 연결된 코어 수가 같으면 최소 길이 선택
+                minLen = Math.min(minLen, wireLen);
+            }
+            return;
+        }
+
+        // 선택되지 않은 core는 패스 
+        if (!selected[index]) {
+        	check(index + 1, connected, wireLen, selected);
+            return;
+        }
+
+        int r = cores.get(index).r;
+        int c = cores.get(index).c;
+
+        for (int i = 0; i < 4; i++) {
+            int x = r;
+            int y = c;
+            int len = 0;
+            boolean canConnect = true;
+
+            while (true) {
+                x += dx[i];
+                y += dy[i];
+
+                if (x < 0 || y < 0 || x >= N || y >= N) break;
+                // 가는 방향에 코어가 있거나 이미 전선이 있는 경우 연결할 수 없음
+                if (arr[x][y] != 0) {
+                    canConnect = false;
+                    break;
+                }
+                len++;
+            }
+            // 연결 가능한 경우
+            if (canConnect) {
+                x = r;
+                y = c;
+                // 연결한 부분을 -1로 채우기
+                for (int j = 0; j < len; j++) {
+                    x += dx[i];
+                    y += dy[i];
+                    arr[x][y] = -1;
+                }
+
+                // 다음 코어로 이동
+                check(index + 1, connected + 1, wireLen + len, selected);
+
+                // 원상복구
+                x = r;
+                y = c;
+                for (int j = 0; j < len; j++) {
+                    x += dx[i];
+                    y += dy[i];
+                    arr[x][y] = 0;
+                }
+            }
+        }
+        // 현재 코어를 연결하지 않고 진행
+        check(index + 1, connected, wireLen, selected);
+    }
 }
