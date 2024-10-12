56e670d6ad7fd97a5d0d641b415c5155d8265444
taeyoung <teayoung0812@naver.com>
2024-10-12 22:51:12 +0900

    1012

--- Study/src/s10/s1011/BOJ_5427.java
+++ Study/src/s10/s1011/BOJ_5427.java
@@ -4,72 +4,113 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_5427 {
-	
-	static int w,h;
-	static char[][] map;
-	static int startR, startC;
-	
-	/*
-	. : 빈 공간
-	# : 벽
-	@ : 상근이의 시작 위치
-	* : 불
-	 
-	q 담기, 
-	list 담기 -> 개수 세기
-	=> 불 퍼짐
-	
-	옮겨진 칸 or 불이 붙으려는 칸
-	이동할 칸 + 이동할 칸의 4방향 모두 불이 없음 -> 상근이 이동
-	
-	 */
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc=1;tc<=T;tc++) {
-			st = new StringTokenizer(br.readLine());
-			w = Integer.parseInt(st.nextToken());  // 너비, 열
-			h = Integer.parseInt(st.nextToken());  // 높이, 행
-			map = new char[h][w];
-			
-			// map 생성
-			for(int r=0;r<h;r++) {
-				String str = br.readLine();
-				for(int c=0;c<w;c++) {
-					map[r][c] = str.charAt(c);
-					// 상근이의 시작 위치
-					if(map[r][c]=='@') {
-						startR = r;
-						startC = c;
-					}
-				}
-			}
-			int time=0;
-			while(true) {
-				move();
-				
-				fire();
-				time++;
-			}
-			
-			
-			
-		}
+    static int w, h;
+    static char[][] map;
+    static int[] dx = {-1, 1, 0, 0}; 
+    static int[] dy = {0, 0, -1, 1};
 
-	}
+    static class Node {
+        int x, y;
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-	private static void fire() {
-		// TODO Auto-generated method stub
-		
-	}
+    static class Info {
+        int x, y, time;
+        Info(int x, int y, int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+    }
 
-	private static void move() {
-		// TODO Auto-generated method stub
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());  // 너비
+            h = Integer.parseInt(st.nextToken());  // 높이
+            map = new char[h][w];
+
+            Queue<Node> fireQueue = new LinkedList<>();
+            Queue<Info> personQueue = new LinkedList<>();
+
+            for (int r = 0; r < h; r++) {
+                String line = br.readLine();
+                for (int c = 0; c < w; c++) {
+                    map[r][c] = line.charAt(c);
+                    if (map[r][c] == '@') {  // 상근이 위치
+                        personQueue.add(new Info(r, c, 0));  
+                        map[r][c] = '.'; 
+                    } else if (map[r][c] == '*') {  // 불의 위치
+                        fireQueue.add(new Node(r, c));
+                    }
+                }
+            }
+
+            int result = bfs(personQueue, fireQueue);  
+            if (result == -1) {
+                System.out.println("IMPOSSIBLE");
+            } else {
+                System.out.println(result);
+            }
+        }
+    }
+
+    private static int bfs(Queue<Info> personQueue, Queue<Node> fireQueue) {
+        boolean[][] visited = new boolean[h][w];
+
+        while (!personQueue.isEmpty()) {
+
+            // 불 확산
+            int fireSize = fireQueue.size();
+            for (int i = 0; i < fireSize; i++) {
+                Node fire = fireQueue.poll();
+                for (int d = 0; d < 4; d++) {
+                    int nx = fire.x + dx[d];
+                    int ny = fire.y + dy[d];
+
+                    if (nx >= 0 && nx < h && ny >= 0 && ny < w && map[nx][ny] == '.') {
+                        map[nx][ny] = '*';
+                        fireQueue.add(new Node(nx, ny));
+                    }
+                }
+            }
+
+            // 상근이 이동
+            int personSize = personQueue.size();
+            for (int i = 0; i < personSize; i++) {
+                Info person = personQueue.poll();
+                int x = person.x;
+                int y = person.y;
+                int time = person.time;
+
+                // 상근이가 건물 밖으로 나가면 탈출 성공
+                if (x == 0 || x == h - 1 || y == 0 || y == w - 1) {
+                    return time + 1;  
+                }
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+
+                    if (nx >= 0 && nx < h && ny >= 0 && ny < w && map[nx][ny] == '.' && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        personQueue.add(new Info(nx, ny, time + 1));
+                    }
+                }
+            }
+        }
+
+        return -1; 
+    }
 }
+
+
