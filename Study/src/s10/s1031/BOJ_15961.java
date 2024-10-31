bf58a52946bf92d7b71354f3c138bd552fd57832
taeyoung <teayoung0812@naver.com>
2024-10-31 23:55:27 +0900

    1031 home

---
+++ Study/src/s10/s1031/BOJ_15961.java
@@ -0,0 +1,22 @@
+package s10.s1031;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_15961 {
+	
+	static int N,d,k,c;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());  // 접시의 수
+		d = Integer.parseInt(st.nextToken());  // 초밥의 가짓수
+		k = Integer.parseInt(st.nextToken());  // 연속해서 먹는 접시의 수
+		c = Integer.parseInt(st.nextToken());  // 쿠폰번호
+		
+		
+		
+	}
+
+}
