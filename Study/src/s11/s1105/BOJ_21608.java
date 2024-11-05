2c5d6880667376a8f7b05ce8be7549a3f5d51cca
0taetae <teayoung0812@naver.com>
2024-11-05 17:52:55 +0900

    1105

---
+++ Study/src/s11/s1105/BOJ_21608.java
@@ -0,0 +1,19 @@
+package s11.s1105;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_21608 {
+	
+	static int N;
+	static int[][] arr;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		
+
+	}
+
+}
